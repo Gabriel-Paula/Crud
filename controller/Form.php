c9a0f1d2d9c428a99e5303c44f09b4b3713a9397
Alunos <gabrieldepaula0512@gmail.com>
2022-05-03 08:42:04 -0300

    auao

--- controller/Form.php
+++ controller/Form.php
@@ -2,6 +2,7 @@
 class Form
 {
   private $message = "";
+  private $error = "";
   public function __construct()
   {
     Transaction::open();
@@ -9,6 +10,10 @@ class Form
   public function controller()
   {
     $form = new Template("view/form.html");
+    $form->set("id", "");
+    $form->set("titulo", "");
+    $form->set("release", "");
+    $form->set("local", "");
     $this->message = $form->saida();
   }
   public function salvar()
@@ -20,15 +25,68 @@ class Form
         $titulo = $conexao->quote($_POST["titulo"]);
         $release = $conexao->quote($_POST["release"]);
         $local = $conexao->quote($_POST["local"]);
-        $resultado = $noticia->insert("titulo, `release`, local", "$titulo, $release, $local");
+        if (empty($_POST["id"])) {
+          $noticia->insert(
+            "titulo, `release`, local",
+            "$titulo, $release, $local"
+          );
+        } else {
+          $id = $conexao->quote($_POST["id"]);
+          $noticia->update(
+            "titulo = $titulo,`release` = $release, local = $local",
+            "id = $id"
+          );
+        }
+        $this->message = $noticia->getMessage();
+        $this->error = $noticia->getError();
       } catch (Exception $e) {
-        echo $e->getMessage();
+        $this->message = $e->getMessage();
+        $this->error = true;
+      }
+    }else {
+      $this->message = "Campos não informados!";
+      $this->error = true;
+    }
+  }
+  public function editar()
+  {
+    if (isset($_GET["id"])) {
+      try {
+        $conexao = Transaction::get();
+        $id = $conexao->quote($_GET["id"]);
+        $noticia = new Crud("noticia");
+        $resultado = $noticia->select("*", "id = $id");
+        if (!$noticia->getError()) {
+        $form = new Template("view/form.html");
+        foreach ($resultado[0] as $cod => $valor) {
+          $form->set($cod, $valor);
+        }
+        $this->message = $form->saida();
+        }else {
+          $this->message = $noticia->getMessage();
+          $this->error = true;
+        }
+      } catch (Exception $e) {
+        $this->message = $e->getMessage();
+        $this->error = true;
       }
     }
   }
   public function getMessage()
   {
-    return $this->message;
+    if (is_string($this->error)) {
+      return $this->message;
+    } else {
+      $msg = new Template("view/msg.html");
+      if ($this->error) {
+        $msg->set("cor", "danger");
+      } else {
+        $msg->set("cor", "success");
+      }
+      $msg->set("msg", $this->message);
+      $msg->set("uri", "?class=Tabela");
+      return $msg->saida();
+    }
   }
   public function __destruct()
   {
